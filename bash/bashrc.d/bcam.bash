042e2ea5efee68e6a1da9ffad9c67d38c41553da
Brad Powers <powers.bradley@gmail.com>
2020-08-04 11:17:18 -0500

    Remove old Locus junk.

--- bash/bashrc.d/bcam.bash
+++
@@ -1,7 +0,0 @@
-vlc_cmd="vlc --no-audio"                                                                                                                                                                      
-cam_queue="rtsp://10.31.27.11:554//Streaming/Channels/1"                                                                                                                                      
-cam_packing="rtsp://10.31.27.12:554//Streaming/Channels/1"                                                                                                                                    
-bcam() {                                                                                                                                                                                      
-  $vlc_cmd $cam_queue & $vlc_cmd $cam_packing &                                                                                                                                               
-}  
-
